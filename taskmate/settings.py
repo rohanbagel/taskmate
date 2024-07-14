7e75a9680ac562cbbc161d7277d265ae265de9a7
Rohanbagel <gitgud1211@gmail.com>
2024-07-14 17:11:18 +0530

    railway updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,8 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,6 +142,6 @@ STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK ="bootstrap5"
-CRIPSY_ALLOWED_TEMPLATE_PACKS="bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS="bootstrap5"
 LOGIN_REDIRECT_URL ="todolist" 
 LOGIN_URL="login"
